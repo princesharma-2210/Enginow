66621dcf69a7dc5e36d177a0d01466118d7d61e1
princesharma-2210 <shaarmprince@gmail.com>
2025-10-07 06:25:47 +0530

    hange in payment section

--- app/api/payment/page.ts
+++ app/api/payment/page.ts
@@ -1,17 +1,17 @@
-import Razorpay from "razorpay";
+// import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET!,
-});
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID!,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET!,
+// });
 
-export default async function handler(req, res) {
-  const { amount, courseId } = req.body;
-  const options = {
-    amount: amount * 100,
-    currency: "INR",
-    receipt: courseId,
-  };
-  const order = await razorpay.orders.create(options);
-  res.status(200).json(order);
-}
+// export default async function handler(req, res) {
+//   const { amount, courseId } = req.body;
+//   const options = {
+//     amount: amount * 100,
+//     currency: "INR",
+//     receipt: courseId,
+//   };
+//   const order = await razorpay.orders.create(options);
+//   res.status(200).json(order);
+// }
